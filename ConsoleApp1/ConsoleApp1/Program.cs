a72f51f0d71bdb36fc6979574d46ef4d994d9ea3
Mihaelist <73944782+Mihaelist@users.noreply.github.com>
2021-10-12 01:28:22 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Введите количество элементов массива: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                double[] a = new double[b];
+                int n = a.Length;
+                string str;
+                int i;
+                double sum = 0;
+                Random rnd = new Random();
+                for (i = 0; i < n; i++)
+                {
+                    a[i] = rnd.Next(-100, 100);
+                    Console.WriteLine(a[i]);
+                    sum += a[i];
+                }
+                double avg;
+                avg = sum / n;
+                str = "Сумма массива равна " + sum + "\nСреднее арифметическое массива равно  " + avg;
+                Console.WriteLine(str);
+                Console.WriteLine("Нажмите любую кнопку!");
+                Console.ReadKey();
+            
+        }
+    }
+}
